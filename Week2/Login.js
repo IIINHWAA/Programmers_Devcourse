f19149ccbdbb310949604cce810c17f66dbf1670
inhwa <dlsghk0711@naver.com>
2024-04-17 13:27:51 +0900

    login

---
+++ Week2/Login.js
@@ -0,0 +1,15 @@
+
+function popId(){
+    if (document.getElementById('txt_id').value==""){
+        alert('아이디를 입력해주세요');
+    }
+    else{
+        alert(document.getElementById('txt_id').value);
+    }            
+}
+
+function myFuction(){
+    alert('1');
+    alert('2');
+    alert('3');
+}
