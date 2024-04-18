9b9283e316e5db72e10c2a7fedaecf9eb1f47da3
inhwa <dlsghk0711@naver.com>
2024-04-18 22:07:34 +0900

    Simple_Market

---
+++ Week2/Simple_Market/orderlist.js
@@ -0,0 +1,19 @@
+let list = [];
+function order(num){
+
+    if(num==1){
+        alert('피자가 주문 목록에 추가되었습니다.');
+        list.push("Pizza");
+    }
+
+    if(num==2){
+        alert('치킨이 주문 목록에 추가되었습니다.');
+        list.push("Chicken");
+    }
+
+    if(num==3){
+        alert('햄버거가 주문 목록에 추가되었습니다.');
+        list.push("Hamburger");
+    }
+}
+
