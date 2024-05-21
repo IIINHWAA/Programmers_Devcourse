bfc9f2cceb80eac0ba8e1fb632678f841fccfc98
inhwa <dlsghk0711@naver.com>
2024-05-21 18:13:04 +0900

    delete

--- secondHandAPI/app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-app.listen(1212)
-
-const usersRouter = require('./routes/users')
-const productsRouter = require('./routes/product')
-app.use("/", usersRouter)
-app.use("/", productsRouter)
